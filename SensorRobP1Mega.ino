ada17d12361a818d1178c2c8d59fb9bd878006fe
JPHammer <72764222+JPHammer@users.noreply.github.com>
2020-11-09 17:20:26 +0100

    Firmware for Arduino Mega 2560 in sensor circuit
    Pins integrated

---
+++ SensorRobP1Mega.ino
@@ -0,0 +1,55 @@
+//----------------//----------------//----------------//----------------//----------------//
+//Global variables//Global variables//Global variables//Global variables//Global variables//
+//----------------//----------------//----------------//----------------//----------------//
+int sensor1HL = D38;
+int signal1HL = D39;
+int sensor2HL = D40;
+int signal2HL = D41;
+int sensor3HL = D42;
+int signal3HL = D43;
+int imOkWire = D44;
+int sensorNumGRP1 = D45;
+int sensorNumGRP2 = D46;
+int sensorErrorType = D47;
+
+//------------//------------//------------//------------//------------//------------//------------//
+//Global setup//Global setup//Global setup//Global setup//Global setup//Global setup//Global setup//
+//------------//------------//------------//------------//------------//------------//------------//
+
+void setup() {
+  sensor1HL == 0;
+  signal1HL == 0;
+  sensor2HL == 0;
+  signal2HL == 0;
+  sensor3HL == 0;
+  signal3HL == 0;
+  imOkWire == 0;
+  sensorNumGRP2 == 0;
+  sensorNumGRP2 == 0;
+  sensorErrorType == 0;
+
+  pinMode (sensor1HL, OUTPUT);
+  pinMode (signal1HL, INPUT);
+  pinMode (sensor2HL, OUTPUT);
+  pinMode (signal2HL, INPUT);
+  pinMode (sensor3HL, OUTPUT);
+  pinMode (signal3HL, INPUT);
+  pinMode (imOkWire, INPUT);
+  pinMode (sensorNumGRP1, INPUT);
+  pinMode (sensorNumGRP2, INPUT);
+  pinMode (sensorErrorType, INPUT);
+
+//----//----//----//----//----//----//----//----//----//----//----//----//----//----//----//----//----//
+//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//Main//
+//----//----//----//----//----//----//----//----//----//----//----//----//----//----//----//----//----//
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//------------------//------------------//------------------//------------------//------------------//
+//Function libraries//Function libraries//Function libraries//Function libraries//Function libraries//
+//------------------//------------------//------------------//------------------//------------------//
